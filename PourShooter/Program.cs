7b6eddb1bbc104266ffba04e4d3b26f21f2f3f0e
Evan Olson <olsone@corning.com>
2022-08-25 09:14:14 -0400

    initial commit

---
+++ PourShooter/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Shooters?");
+string response = Console.ReadLine();
+
+while (!IsValid(response))
+{
+    Console.WriteLine("Please Enter valid response.");
+    Console.WriteLine("Valid Reponses:");
+    Console.WriteLine("OH HELL YES!");
+
+    Console.Write("Response: ");
+    response = Console.ReadLine();
+}
+
+
+PourShooter();
+
+Console.Read();
+
+
+bool IsValid(string input)
+{
+    return input != "OH HELL YES!" ? false : true;
+}
+
+void PourShooter()
+{
+    Console.WriteLine("                                                                                \r\n                                                                                \r\n                                                                                \r\n                                                                                \r\n                                                                                \r\n                                                                                \r\n                                                                                \r\n                                                                          ,#@@@@\r\n                                      .**/,/*/,. *. *  ,       .,.,,*(#(**//((((\r\n                                      ,*//##*/(*((///(##%(%##(////((((//*,...,,*\r\n                                     ,  .,,(  , ,  ,* /.....         .,,,,****//\r\n                                     ,  .. *  ..(**(,/&(((*..     ..,,*,,,,.....\r\n                                     /(/*,.,    ,   . / .         ..,,*,,,,,,,..\r\n                                   (/,*/*,,* .  ,    .* , .........,*****,,,,,,,\r\n                                    /##,((((,/*/*,.,*(*.***/****,,,*((//****,,,,\r\n                               .    .  ..,(#/((#*. (#& ./(/########%&%#((///****\r\n                             ,/   *,                           .,(##%&&&%%#((((/\r\n                            (     .                                   ,(#(%%(#(%\r\n                           /     /,                                             \r\n                          .    #...                                             \r\n                          .   ,*%                                               \r\n                         #    ,.                                                \r\n                        *    .                                                  \r\n                        .    .                                                  \r\n                       .   .**                                                  \r\n                       .   . *                                                  \r\n                          . ,/                                                  \r\n                       .   **.                                                  \r\n                      /    ,                                                    \r\n                         * .                                                    \r\n            .////*///////#******/(#(//****//.                                   \r\n            .%(*,...*.. .&   ,(&%(/(%(/.,,((*                                   \r\n             (/(*#* ,.**@..(#%%(////,.*(.//(.                                   \r\n             /(%((%(/#((*((/###%#%#/((#(#%#(.                                   \r\n             ,*/,...(*(****/.,*.,*/...,,.,*(                                    \r\n              * *  .*#/./(**#//,/,(..,....*,                                    \r\n              .*#%,**.////*.,*.*.*/..**,#,%                                     \r\n               */@.,/*(//*(.&#((....,*,/(#                                      \r\n               *(*%/(#%##(#((#/,*.,,,,,#*##                                     \r\n               ,*/@.((/*(((//(,.,,,,,,,#/&/                                     \r\n               ./,/#**/(###(,(,#(//*///(/#                                      \r\n                (..&#**///******//#((/#,##                                      \r\n                (/ .  ........    .,.,,,#%                                      \r\n                *(........       .,*.,,*#&                                      \r\n                ,*.........      .,/.,,/%(                                      \r\n                ,%#**//(#%#%%%%####%*/#%#.                                      \r\n                ,*                ., ...//                                      \r\n                ,,                 .   .**                                      \r\n");
+}
+
+
+
